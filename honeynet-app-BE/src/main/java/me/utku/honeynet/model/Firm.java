d6dd689ec422574c9c67fa452cde51db7eae6a93
utkuonursahin <sahinutkuonur@gmail.com>
2023-07-27 17:39:34 +0300

    before typealias

--- honeynet-app-BE/src/main/java/me/utku/honeynet/model/Firm.java
+++ honeynet-app-BE/src/main/java/me/utku/honeynet/model/Firm.java
@@ -1,12 +1,14 @@
 package me.utku.honeynet.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
 @Document
+@TypeAlias("firm")
 public class Firm extends Base {
     private String firmName;
     private String previewImgPath;
