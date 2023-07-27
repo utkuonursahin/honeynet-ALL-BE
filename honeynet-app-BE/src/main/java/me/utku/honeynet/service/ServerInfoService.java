696e3dc263e43d606753595c9a799b43115a21fe
utkuonursahin <sahinutkuonur@gmail.com>
2023-07-27 13:56:05 +0300

    implement base backend port finder mechanism

--- honeynet-app-BE/src/main/java/me/utku/honeynet/service/ServerInfoService.java
+++ honeynet-app-BE/src/main/java/me/utku/honeynet/service/ServerInfoService.java
@@ -8,6 +8,7 @@ import me.utku.honeynet.model.ServerInfo;
 import me.utku.honeynet.repository.ServerInfoRepository;
 import org.springframework.stereotype.Service;
 
+import java.net.ServerSocket;
 import java.util.UUID;
 
 @Slf4j
@@ -16,13 +17,25 @@ import java.util.UUID;
 public class ServerInfoService {
     private final ServerInfoRepository serverInfoRepository;
 
+    public int findAvailablePort(){
+        try{
+            ServerSocket serverSocket = new ServerSocket(0);
+            int port = serverSocket.getLocalPort();
+            serverSocket.close();
+            return port;
+        } catch (Exception exception){
+            log.error("ServerInfo service findAvailablePort exception: {}",exception.getMessage());
+            return 0;
+        }
+    }
+
     public ServerInfo create(Pot pot, Firm firm){
         ServerInfo serverInfo = new ServerInfo();
         try{
             serverInfo.setId(UUID.randomUUID().toString());
             serverInfo.setPot(pot);
             serverInfo.setFirm(firm);
-            serverInfo.setPort("8090");
+            serverInfo.setPort(Integer.toString(findAvailablePort()));
             Boolean isExists = serverInfoRepository.existsServerInfosByPotIdAndFirmId(pot.getId(),firm.getId());
             if(!isExists){
                 serverInfo = serverInfoRepository.save(serverInfo);
